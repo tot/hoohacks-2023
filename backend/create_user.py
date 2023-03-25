c9c57beacb7dca49967380ad434c0a607d361bb6
chengJazhua <chengjazhua@gmail.com>
2023-03-25 17:40:04 -0400

    create transaction + user working

--- backend/create_user.py
+++ backend/create_user.py
@@ -14,9 +14,42 @@ def get_customers():
     headers={'Accept': 'application/json'},
     )._content)
 
+#Get List of Accounts for customer
+def get_accounts(id):
+    url = 'http://api.nessieisreal.com/customers/' + id+ '/accounts?key={}'.format(api_key)
+    return json.loads(requests.get(url, headers={ 'Accept': 'application/json'}, )._content)
+
+
+url = 'http://api.nessieisreal.com/data?type=customers?key={}'.format(api_key)
+response = requests.delete( 
+	url, 
+	headers={ 'Accept': 'application/json'},
+)
+print(response.content)
+
+url = 'http://api.nessieisreal.com/customers?key={}'.format(api_key)
+payload = {
+  "first_name": "John",
+  "last_name": "Doe",
+  "address": {
+    "street_number": "100",
+    "street_name": "University Avenue",
+    "city": "Charlottesville",
+    "state": "VA",
+    "zip": "22901"
+  }
+}
+
+# Create User
+response = requests.post( 
+	url, 
+	data=json.dumps(payload),
+	headers={'Content-Type': 'application/json', 'Accept': 'application/json'},
+)
+print(response.content)
+
 for customer in get_customers():
     customer_id = customer['_id']
-    print(customer_id)
 
 
     #Create Accounts
@@ -31,10 +64,10 @@ for customer in get_customers():
 
 
     #Create Credit Card Account
-    #response = requests.post(url,data=json.dumps(payload), 
-        #headers={'Content-Type': 'application/json', 'Accept': 'application/json'} )
+    response = requests.post(url,data=json.dumps(payload), 
+        headers={'Content-Type': 'application/json', 'Accept': 'application/json'} )
 
-    #print (response.content)
+    print (response.content)
 
     payload = {
     "type": "Checking",
@@ -44,23 +77,19 @@ for customer in get_customers():
     }
 
     #Create Other Accounts
-    #response = requests.post(url,data=json.dumps(payload), 
-       # headers={'Content-Type': 'application/json', 'Accept': 'application/json'} )
-
-    #print (response.content)
-
-
-    url = 'http://api.nessieisreal.com/accounts?key={}'.format(api_key)
-    response = requests.get(url, headers={ 'Accept': 'application/json'} )
+    response = requests.post(url,data=json.dumps(payload), 
+        headers={'Content-Type': 'application/json', 'Accept': 'application/json'} )
 
     print (response.content)
 
 
-    #Create Transactions
-
-
-
-    print(create_transactions.createDummyTransaction(api_key, customer_id).content)
+    for i in range(50):
+        for account in get_accounts(customer_id):
+            print(account)
+            account_id = account['_id']
+            
+            #Create Transactions
+            print(create_transactions.createDummyTransaction(api_key, account_id).content)
 
 
 
