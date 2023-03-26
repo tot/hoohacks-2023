a94bbe8d46034373fa792adebe228dd046aa50f8
Michael Shi <mfshi@Michaels-MacBook-Air.local>
2023-03-25 23:02:31 -0400

    date processing and subscriptions

--- backend/process_data.py
+++ backend/process_data.py
@@ -1,13 +1,16 @@
 from dotenv import load_dotenv
 from datetime import datetime
 from pymongo import MongoClient
+from enum import Enum
 import os, requests, json
 
 load_dotenv()
 api_key = os.getenv('API_KEY')
 db_url = os.getenv('DB_URL')
-db = MongoClient(db_url)
+db = MongoClient(db_url)['admin']
 users = db['users']
+subscriptions = db['subscriptions']
+
 
 def get_customers():
     return json.loads(requests.get( 
@@ -62,3 +65,127 @@ def process_customer_data(customer_id):
             transactions = get_transactions(account['_id'])
             process_statistics(user, last_accessed, account, transactions)
             log_transactions(transactions)
+
+class TimeInterval(Enum):
+    ONE_YEAR = 365 * 24 * 3600
+    SIX_MONTHS = 6 * 30 * 24 * 3600
+    ONE_MONTH = 30 * 24 * 3600
+'''
+write db code to check if to datetime strings are exactly the interval apart 
+the dates are not necessarily in order
+also make enums to represent common intervals like a year, 6-months, and one month
+'''
+def checkInterval(date1: str, date2: str, interval: TimeInterval) -> bool:
+    date_format = "%Y-%m-%d %H:%M:%S.%f"
+    datetime1 = datetime.strptime(date1, date_format)
+    datetime2 = datetime.strptime(date2, date_format)
+    delta = abs(datetime1 - datetime2)
+    return delta.total_seconds() == interval.value
+
+def mostRecent(date1: str, date2: str) -> str:
+    date_format = "%Y-%m-%d %H:%M:%S.%f"
+    datetime1 = datetime.strptime(date1, date_format)
+    datetime2 = datetime.strptime(date2, date_format)
+
+    if datetime1 >= datetime2:
+        return datetime1.strftime(date_format)
+    else:
+        return datetime2.strftime(date_format)
+
+'''
+write a method that takes date stringss and returns the most recent
+'''
+
+def count_subscriptions(transactions):
+    for i, transaction in enumerate(transactions):
+        date = transaction["purchase_date"]
+        name = transaction["merchant_name"]
+        id = transaction["merchant_id"]
+        cost = transaction["amount"]
+        merchants = []
+        for j in range(i, len(transactions)):
+            next_date = transactions[j]["purchase_date"]
+            next_name = transactions[j]["merchant_name"]
+            if name not in merchants and (checkInterval(date, next_date, TimeInterval.ONE_MONTH) or checkInterval(date, next_date, TimeInterval.SIX_MONTHS) or checkInterval(date, next_date, TimeInterval.ONE_YEAR)):
+                db["subscriptions"].insert_one({
+                    "merchant_name": next_name,
+                    "merchant_id": id,
+                    "cost": cost,
+                    "last_renewal":mostRecent(date, next_date)
+                })
+                merchants.append(name)
+                break
+
+test_transactions = [
+    {
+        "purchase_date": "2021-01-01 00:00:00.000",
+        "merchant_name": "Netflix",
+        "merchant_id": 1,
+        "amount": 10
+    },
+    {
+        "purchase_date": "2021-01-31 00:00:00.000",
+        "merchant_name": "Netflix",
+        "merchant_id": 1,
+        "amount": 10
+    },
+    {
+        "purchase_date": "2021-02-28 00:00:00.000",
+        "merchant_name": "Netflix",
+        "merchant_id": 1,
+        "amount": 10
+    },
+    {
+        "purchase_date": "2021-03-01 00:00:00.000",
+        "merchant_name": "Hulu",
+        "merchant_id": 2,
+        "amount": 20
+    },
+    {
+        "purchase_date": "2021-04-01 00:00:00.000",
+        "merchant_name": "Hulu",
+        "merchant_id": 2,
+        "amount": 20
+    },
+    {
+        "purchase_date": "2021-05-01 00:00:00.000",
+        "merchant_name": "Amazon",
+        "merchant_id": 3,
+        "amount": 40
+    }
+]
+
+count_subscriptions(test_transactions)
+'''
+Based on the schema where txns stands for transactions, add subscriptions by finding two transactions where the merchant_id
+is the same and the purchase_date is a datetime string that happens on the same day of the month
+txns {
+    id:str
+    mearchts_id:str
+    purchase_date 
+    amount: 
+    description
+
+}
+
+subscription
+{
+    id: str
+    merchant_name : str 
+    merchant_id: str
+    cost: str
+
+}
+
+use this format to add to a subscription
+db["subscription"].insert_one
+
+and loop through these transactions to find transaction with purchase dates on the same day of the month
+def log_transactions(transactions):
+    for transaction in transactions:
+        t_id, m_id, b_id = transaction['_id'], transaction['merchant_id'], transaction['payer_id']
+        date, amt, descr = transaction['purchase_date'], transaction['amount'], transaction['description']
+        db['txns'].insert_one({'id': t_id, 'merchant_id': m_id, 'buyer_id': b_id, 'purchase_date': date, 'amount': amt, 'description': descr})
+
+
+'''
